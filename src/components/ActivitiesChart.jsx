a24da4ef2214c7534df0711d0551464d02034e81
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-05 01:10:37 +0530

    some basic chart styling

--- src/components/ActivitiesChart.jsx
+++ src/components/ActivitiesChart.jsx
@@ -1,6 +1,5 @@
-// src/components/ActivitiesChart.js
-import React, { useMemo } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { useMemo } from "react";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,32 +8,80 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const ActivitiesChart = React.memo(() => {
-  const data = useMemo(() => ({
-    labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-    datasets: [
-      {
-        label: 'Guest',
-        data: [400, 300, 200, 300],
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1,
-      },
-      {
-        label: 'User',
-        data: [200, 400, 300, 400],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-      },
-    ],
-  }), []);
+  const data = useMemo(
+    () => ({
+      labels: ["", "Week 1", "", "Week 2", "", "Week 3", "", "Week 4"],
+      datasets: [
+        {
+          label: "Guest",
+          data: [0, 500, 0, 350, 0, 200, 0, 400],
+          backgroundColor: "#98d89e",
+          // categoryPercentage: 0.9,
+          borderRadius: 5,
+          barThickness: 40,
+          // categorySpacing: 0,
+          // barPercentage: 0.9,
+        },
+        {
+          label: "User",
+          data: [0, 400, 0, 450, 0, 300, 0, 350],
+          backgroundColor: "#ee8484",
+          // categoryPercentage: 0.9,
+          // barPercentage: 0.9,
+          borderRadius: 5,
+          barThickness: 40,
+        },
+      ],
+    }),
+    []
+  );
 
-  return <Bar data={data} />;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: "top",
+        align: "end",
+        labels: {
+          usePointStyle: true,
+          pointStyle: "circle",
+        },
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          stepSize: 100,
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      xAxes: [
+        {
+          maxBarThickness: 100,
+          
+        },
+      ],
+    },
+  };
+
+  return <Bar data={data} options={options} />;
 });
 
 export default ActivitiesChart;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,34 @@
-import React, { Suspense } from 'react';
-import Sidebar from './Sidebar';
-import ActivitiesChart from './ActivitiesChart'; // Assuming direct import for simplicity
-import TopProductsChart from './TopProductsChart'; // Assuming direct import for simplicity
+import React, { Suspense } from "react";
+import Sidebar from "./Sidebar";
+import ActivitiesChart from "./ActivitiesChart";
+import TopProductsChart from "./TopProductsChart";
 
 const Dashboard = () => {
   const metrics = [
-    { icon: 'assets/revenue_icon.png', name: 'Total Revenues', value: '$2,129,430', percentage: 4 },
-    { icon: 'assets/transactions_icon.png', name: 'Total Transactions', value: '1,520', percentage: 2 },
-    { icon: 'assets/likes_icon.png', name: 'Total Likes', value: '9,721', percentage: 8 },
-    { icon: 'assets/users_icon.png', name: 'Total Users', value: '9,721', percentage: -5 },
+    {
+      icon: "assets/revenue_icon.png",
+      name: "Total Revenues",
+      value: "$2,129,430",
+      percentage: 4,
+    },
+    {
+      icon: "assets/transactions_icon.png",
+      name: "Total Transactions",
+      value: "1,520",
+      percentage: 2,
+    },
+    {
+      icon: "assets/likes_icon.png",
+      name: "Total Likes",
+      value: "9,721",
+      percentage: 8,
+    },
+    {
+      icon: "assets/users_icon.png",
+      name: "Total Users",
+      value: "9,721",
+      percentage: -5,
+    },
   ];
 
   return (
@@ -20,12 +40,45 @@ const Dashboard = () => {
           <div className="search-profile">
             <div className="search-input-container">
               <input type="text" placeholder="Search..." />
-              <img src="assets/search_icon.svg" alt="Search" className="search-icon" />
+              <img
+                src="assets/search_icon.svg"
+                alt="Search"
+                className="search-icon"
+              />
             </div>
             <img src="assets/notification_icon.svg" alt="Notifications" />
             <img src="assets/profile.png" alt="Profile" />
           </div>
         </header>
+        <div className="metrics">
+          {metrics.map((metric, index) => (
+            <div className="metric" key={index}>
+              <img
+                src={metric.icon}
+                alt={metric.name}
+                className="metric-icon"
+              />
+              <div className="metric-details">
+                <span className="metric-name">{metric.name}</span>
+                <div className="metric-value-percentage">
+                  <span className="metric-value">{metric.value}</span>
+                  <span
+                    className="metric-percentage"
+                    style={{
+                      color: metric.percentage >= 0 ? "#2ECC71" : "#E74C3C",
+                      borderColor:
+                        metric.percentage >= 0 ? "#2ECC71" : "#E74C3C",
+                    }}
+                  >
+                    {metric.percentage >= 0
+                      ? `+${metric.percentage}%`
+                      : `${metric.percentage}%`}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>{" "}
         <div className="charts-container">
           <div className="chart-fullwidth">
             <ActivitiesChart />
@@ -35,28 +88,6 @@ const Dashboard = () => {
               <TopProductsChart />
             </div>
           </div>
-        </div>
-        <div className="metrics">
-          {metrics.map((metric, index) => (
-            <div className="metric" key={index}>
-              <img src={metric.icon} alt={metric.name} className="metric-icon" />
-              <div className="metric-details">
-                <span className="metric-name">{metric.name}</span>
-                <div className="metric-value-percentage">
-                  <span className="metric-value">{metric.value}</span>
-                  <span
-                    className="metric-percentage"
-                    style={{
-                      color: metric.percentage >= 0 ? '#2ECC71' : '#E74C3C',
-                      borderColor: metric.percentage >= 0 ? '#2ECC71' : '#E74C3C',
-                    }}
-                  >
-                    {metric.percentage >= 0 ? `+${metric.percentage}%` : `${metric.percentage}%`}
-                  </span>
-                </div>
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </div>
